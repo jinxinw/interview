87f9c843c4fef2ab8eefcccf21c11fbd2ae641c7
Victor Jin <victor.jin@oracle.com>
2018-01-28 14:51:32 -0800

    add Global and Local Inversions

---
+++ Interview/src/algorithms/leetcode/GlobalAndLocalInversions.java
@@ -0,0 +1,9 @@
+package algorithms.leetcode;
+
+/**
+ * Created by xinweijin on 1/28/18.
+ * leetcode #775
+ * https://leetcode.com/problems/global-and-local-inversions/description/
+ */
+public class GlobalAndLocalInversions {
+}
